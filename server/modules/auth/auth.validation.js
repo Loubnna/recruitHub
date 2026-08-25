f758c20a5c3a660f4b965950c590aaaa45143028
Loubna <loubnalazhari8@gmail.com>
2026-08-25 23:08:00 +0100

    implement validtaion

--- server/modules/auth/auth.validation.js
+++ server/modules/auth/auth.validation.js
@@ -1,4 +1,15 @@
-import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 import prisma from "../../config/database.js";
+import z from "zod";
 
+
+export const registerScema = z.object({
+    name: z.string().min(2).max(100),
+    email: z.string().email(),
+    password: z.string().min(8).max(100)
+
+});
+export const loginScema = z.object({
+    email: z.string().email(),
+    password: z.string().min(1)
+
+})
