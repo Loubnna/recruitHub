06ca5cb6d0bd099ec607c38071b446df003a9bf9
Loubna <loubnalazhari8@gmail.com>
2026-08-28 21:02:44 +0100

    add the role  authorization level

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import cookieParser from "cookie-parser"
 import authRoutes from "./modules/auth/auth.routes.js";
+import cors from "cors";
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -12,6 +13,12 @@ app.get("/" , (req,res) => {
         message : "welcome to this api"
     });
 });
+app.use(cors({
+    origin:  "http://localhost:3000" ,
+    credentials:true
+
+}));
+app.use(cookieParser());
 
 
 export default app;
