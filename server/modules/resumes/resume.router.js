b7b56e0df21a161c00f2124d8908ece3bbc74dc1
Loubna <loubnalazhari8@gmail.com>
2026-09-19 21:03:17 +0100

    home page implementation

--- server/modules/resumes/resume.router.js
+++ server/modules/resumes/resume.router.js
@@ -5,7 +5,7 @@ import upload from "../../middleware/uploadMiddleware.js";
 
 const router =express.Router();
 
-resumeController.post("/" , authenticate , upload.single("resume" , resumeController.upload));
+router.post("/" , authenticate , upload.single("resume" , resumeController.upload));
 router.get("/me", authenticate, resumeController.getMine);
 router.patch("/", authenticate, upload.single("resume"), resumeController.update);
 router.delete("/", authenticate, resumeController.remove);
